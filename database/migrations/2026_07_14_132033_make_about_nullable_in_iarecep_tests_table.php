ec60a0a995baff6b2750d53806b89635463dd422
Rdelys <rdauphinelys@gmail.com>
2026-07-14 16:21:18 +0300

    test

---
+++ database/migrations/2026_07_14_132033_make_about_nullable_in_iarecep_tests_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+public function up(): void
+{
+    Schema::table('iarecep_tests', function (Blueprint $table) {
+        $table->text('about')->nullable()->change();
+    });
+}
+
+public function down(): void
+{
+    Schema::table('iarecep_tests', function (Blueprint $table) {
+        $table->text('about')->nullable(false)->change();
+    });
+}
+};
